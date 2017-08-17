4a74eaec3a0b293bb7a51d95479faef29d94a981
b0kater <b0kater@yahoo.com>
2017-08-17 16:52:17 -0400

    first REAL commit, added python map and lambda solution

---
+++ python/pythonfunctionals/MapandLambdaFunction.py
@@ -0,0 +1,13 @@
+cube = lambda x: x**3 # complete the lambda function 
+
+def fibonacci(n):
+    # return a list of fibonacci numbers
+    init = [0,1]
+    for i in range(2,n):
+        init.append(init[i-1]+init[i-2])
+    
+    return init[0:n] # in case n is less than 2
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
